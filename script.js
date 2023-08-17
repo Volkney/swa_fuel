a167062cb0f8eaa1efa3844e4464537ecc6ac975
Jorge Tinoco <volkney@Jorges-MacBook-Air.local>
2023-08-17 13:00:03 -0500

    added input text boxes and radio buttons to display correct values depending on aircraft

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-    const inputNumber = document.getElementById('inputNumber');
-    const display1 = document.getElementById('display1');
-    const display2 = document.getElementById('display2');
-    const display3 = document.getElementById('display3');
-    
-    inputNumber.addEventListener('input', () => {
-      const enteredNumber = parseInt(inputNumber.value);
-      if (!isNaN(enteredNumber)) {
-        display1.value = enteredNumber;
-        display2.value = enteredNumber * 2;
-        display3.value = enteredNumber * 3;
-      } else {
-        display1.value = '';
-        display2.value = '';
-        display3.value = '';
-      }
-    });
+const inputNumber = document.getElementById('inputNumber');
+const display1 = document.getElementById('display1');
+const display2 = document.getElementById('display2');
+const display3 = document.getElementById('display3');
+const aircraft = document.querySelectorAll('input[name="group1"]');
+
+var aircraftOne = aircraft[0];
+var aircraftTwo = aircraft[1];
+
+function updateDisplayValues() {
+  const enteredNumber = parseFloat(inputNumber.value);
+  
+  if (aircraftOne.checked) {
+    display1.value = 8.6;
+    display2.value = 8.6;
+    display3.value = enteredNumber - (8.6 + 8.6);
+  } else if (aircraftTwo.checked) {
+    display1.value = 8.5;
+    display2.value = 8.5;
+    display3.value = enteredNumber - (8.5 + 8.5);
+  }
+}
+
+inputNumber.addEventListener('input', updateDisplayValues);
+aircraftOne.addEventListener('change', updateDisplayValues);
+aircraftTwo.addEventListener('change', updateDisplayValues);
