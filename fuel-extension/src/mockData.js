34f126e22e9abebb92c4c8f489091aca549cd06f
Jorge Tinoco <volkney@Jorges-Air-2.lan>
2024-08-03 15:50:48 -0500

    added script to scrape data

---
+++ fuel-extension/src/mockData.js
@@ -0,0 +1,7 @@
+export default {
+    fuel: { minFuel: 30000, maxFuel: 5000 },
+    aircraft: { type: 'Boeing 737', registry: 'N12345' },
+    date: '08/01',
+    flight: { number: '1234', origin: 'LAX', destination: 'JFK' },
+    gate: 15
+  };
--- fuel-extension/vite.config.js
+++ fuel-extension/vite.config.js
@@ -1,12 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        format: 'iife',
+      },
     },
   },
 })
